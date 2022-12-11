f6a01e137c7d86580879fd3cc4477fc4e52659a8
chaeyeon <321codus@naver.com>
2022-12-11 17:53:21 +0900

    fix share

--- js/share.js
+++ js/share.js
@@ -1,12 +1,15 @@
 const url = 'https://mbti-writing-routine-test.netlify.app/';
 
 function setShare(){
-  var resultImg = document.querySelector('#resultImg');
-  var resultAlt = resultImg.firstElementChild.alt;
+  // var resultImg = document.querySelector('#resultImg');
+  // var resultAlt = resultImg.firstElementChild.alt;
   const shareTitle = '유형별 글쓰기 루틴 테스트';
   const shareDes = infoList[resultAlt].name;
-  const shareImage = url + 'img/IMG_080' + resultAlt + '.png';
-  const shareURL = url + 'page/result-' + resultAlt + '.html';
+  var tmp = location.pathname.split("/").pop().toString();
+  tmp = tmp.replace('result-','');
+  tmp = tmp.replace('.html','');
+  const shareImage = url + 'img/IMG_080' + tmp + '.png';
+  const shareURL = url + 'page/result-' + tmp + '.html';
 
   Kakao.Link.sendDefault({
     objectType: 'feed',
